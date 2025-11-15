db18f19953834e6be107d60785496f9379b3402f
marcu <marcusolachan@gmail.com>
2025-11-15 21:24:19 +1100

    rewriting everything

--- basic_intrinsics.h
+++
@@ -1,6 +0,0 @@
-// these functions just call the assembly version
-void _sti();
-
-void _cli();
-
-void _halt();
--- interrupts/load_interrupt_table.asm
+++
@@ -1,7 +0,0 @@
-global load_interrupt_table
-
-
-load_interrupt_table:
-    mov eax, [esp+4]
-    lidt [eax]
-    ret
