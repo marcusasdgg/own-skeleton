91cbd4ccfe6b2479ef3c016de5b6e98cae94015f
jezza <z5560784@ad.unsw.edu.au>
2025-11-24 21:34:27 +1100

    s

--- test.py
+++
@@ -1,6 +0,0 @@
-string = """xorriso -as mkisofs -R -r -J -b boot/limine/limine-bios-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus -apm-block-size 2048 --efi-boot boot/limine/limine-uefi-cd.bin -efi-boot-part --efi-boot-image --protective-msdos-label iso_root -o image.iso"""
-
-stlist = string.split(" ")
-stlist = [f"\"{x}\"" for x in stlist]
-string = ",".join(stlist)
-print(string)
