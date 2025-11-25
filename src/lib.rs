d9ab1580135e3c145038fb1230c08ef24e3f75ba
jezza <z5560784@ad.unsw.edu.au>
2025-11-25 23:56:50 +1100

    draws a line on display hurray

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use core::ptr::write_volatile;
 use core::{panic::PanicInfo, ptr};
 use core::arch::asm;
 mod mem;
@@ -26,15 +27,16 @@ fn panic(_info: &PanicInfo) -> ! {
 
 
 #[unsafe(no_mangle)] 
-pub extern "C" fn _start() -> ! {
-    if limine::limine_header::LIMINE_BASE_REVISION_SUPPORTED(limine::LIMINE_BASE_REVISION) == false {
+pub extern "C" fn kmain() -> ! {
+    
+    unsafe {
+        if limine::limine_header::LIMINE_BASE_REVISION_SUPPORTED(limine::LIMINE_BASE_REVISION) == false {
         unsafe {
             asm!(
                 "hlt"
             )
         }
     }
-    unsafe {
         if LIMINE_FRAMEBUFFER_REQUEST.response == ptr::null_mut() {
             unsafe {
                 asm!(
@@ -45,25 +47,33 @@ pub extern "C" fn _start() -> ! {
         let framebuffer_response: limine_framebuffer_response =  *(LIMINE_FRAMEBUFFER_REQUEST.response);
     
 
-        if framebuffer_response.framebuffer_count < 1 {
-            unsafe {
-                asm!(
-                    "hlt"
-                )
-            }
-        }
+        // if framebuffer_response.framebuffer_count < 1 {
+        //     unsafe {
+        //         asm!(
+        //             "hlt"
+        //         )
+        //     }
+        // }
 
 
         let frame_buffers: *mut *mut limine_framebuffer = framebuffer_response.framebuffers;
 
-        let first_buffer: *mut limine_framebuffer = *(frame_buffers);
+        let first_buffer: limine_framebuffer = **(frame_buffers);
 
+        let buffer_base_address: *mut u32 = first_buffer.address as *mut u32;
+
+        let colo: u32 = 0x00FFFFFF;
+        let pitch: u64 = first_buffer.pitch;
+
+        let pitch_bytes = first_buffer.pitch as usize;
+        let pixels_per_row = pitch_bytes / 4;
         
+        for i in 0..100 {
+            let offset: usize = i * pixels_per_row + i;
+            write_volatile(buffer_base_address.add(offset), colo);
+        }
 
-    
 
-
-    
     }
     loop {}
 }
--- src/limine/mod.rs
+++ src/limine/mod.rs
@@ -6,11 +6,14 @@ use limine_header::LIMINE_FRAMEBUFFER_REQUEST_ID;
 
 #[unsafe(link_section = ".limine_requests")]
 #[used]
-pub static LIMINE_BASE_REVISION: [u64;3] = limine_header::LIMINE_BASE_REVISION(4);
+#[unsafe(no_mangle)]
+pub static mut LIMINE_BASE_REVISION: [u64;3] = limine_header::LIMINE_BASE_REVISION(4);
+
 
 #[unsafe(link_section = ".limine_requests")]
 #[used]
-pub static LIMINE_FRAMEBUFFER_REQUEST: limine_framebuffer_request = limine_header::limine_framebuffer_request { 
+#[unsafe(no_mangle)]
+pub static mut LIMINE_FRAMEBUFFER_REQUEST: limine_framebuffer_request = limine_header::limine_framebuffer_request { 
     id: limine_header::LIMINE_FRAMEBUFFER_REQUEST_ID,
     revision: 0,
     response: core::ptr::null_mut()
@@ -19,10 +22,12 @@ pub static LIMINE_FRAMEBUFFER_REQUEST: limine_framebuffer_request = limine_heade
 
 #[unsafe(link_section = ".limine_requests_start")]
 #[used]
+#[unsafe(no_mangle)]
 pub static LIMINE_REQUESTS_START_MARKER: [u64;4] = limine_header::LIMINE_REQUESTS_START_MARKER;
 
 
 #[unsafe(link_section = ".limine_requests_end")]
 #[used]
+#[unsafe(no_mangle)]
 pub static LIMINE_REQUESTS_END_MARKER: [u64;2] = limine_header::LIMINE_REQUESTS_END_MARKER;
 
